91964cc170aa6438397d55511f430836b677e7c1
Oleksandr Varga <vargasasha@gmail.com>
2023-08-08 22:10:14 +0300

    add transactions component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from "../data/friends.json";
+import transactions from "../data/transactions.json"
 
 
 
@@ -21,6 +23,7 @@ export const App = () => {
       <Statistics stats={data} />
 
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
