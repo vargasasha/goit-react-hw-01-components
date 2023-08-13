965e96e3a241a1529a90776e8b6f85441d402cff
Oleksandr Varga <vargasasha@gmail.com>
2023-08-13 18:52:01 +0300

    component fix

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,12 +1,14 @@
 import { ListItem, List, Text } from "./Statistics.styled";
 
-export const Statistics = ({ stats }) => {
+export const Statistics = ({ stats, title }) => {
  
   return (
     <section>
+
+      {title && <h2>{title}</h2>}
+
       <List>
         {stats.map(item => (
-          
           <ListItem key={item.id}>
             <Text>{item.label}</Text>
             <Text>{item.percentage}%</Text>
